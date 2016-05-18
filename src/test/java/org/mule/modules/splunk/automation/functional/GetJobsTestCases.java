e322d1005426eca8f29c168f32e90f45a1c22d84
victor.regpala <victor.regpala@whiteskylabs.com>
2016-05-18 21:23:49 +0800

    fixed test case

--- src/test/java/org/mule/modules/splunk/automation/functional/GetJobsTestCases.java
+++ src/test/java/org/mule/modules/splunk/automation/functional/GetJobsTestCases.java
@@ -14,15 +14,35 @@ import static org.junit.Assert.assertTrue;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GetJobsTestCases extends SplunkAbstractTestCases {
 
+    private static String SAVED_SEARCH_NAME = "saved_search_demo"; 
+            
+    @Before
+    public void setup() {        
+        try {
+            getConnector().createSavedSearch(SAVED_SEARCH_NAME, "search * | head 10", null);
+            getConnector().runSavedSearch(SAVED_SEARCH_NAME);
+        } catch (Exception e) {
+        }
+    }
+    
+    @After
+    public void tearDown() {
+        try {
+            getConnector().deleteSavedSearch(SAVED_SEARCH_NAME);    
+        } catch(Exception e) {   
+        }
+    }
+    
     @Test
     public void testGetJobs() {
         List<Map<String, Object>> result = getConnector().getJobs();
         assertNotNull(result);
         assertTrue(result.size() > 0);
     }
-
 }
